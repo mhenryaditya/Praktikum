cc06a701ca030f00836d8317c93e64f7760f59eb
mhenryaditya <111096219+mhenryaditya@users.noreply.github.com>
2023-11-27 10:51:50 +0800

    Upload data
    Langsung ke main branch

---
+++ Ucapan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salam;
+
+/**
+ *
+ * @author m_henry_aditya
+ */
+public class Ucapan {
+    public static void main(String[] args) {
+        System.out.println("Halo ULM");
+        System.out.println("ULM Berdaya Saing dan Unggul di Bidang Lahan Basah");
+        System.out.println("Prodi Ilkom ULM Unggul dengan Teknologi Kekinian");
+    }
+}
